8e17143f13f071c40825ede9c651a92f8aa6366f
codexs007 <2165186962@qq.com>
2019-08-05 17:41:09 +0800

    codexs

--- src/modules/wallet/invoice.vue
+++ src/modules/wallet/invoice.vue
@@ -1,83 +1,108 @@
 <template>
-  <div class>
-    <x-header title="发票服务"></x-header>
-    <van-cell-group>
-      <van-cell class="tips">
-        <h4>温馨提示：</h4>
-        <p>一、发票换开：</p>
-        <p>1、订单完成后一年内可支持换开发票一次。</p>
-        <p>2、不支持“企业抬头”变更。</p>
-        <p>二、补开发票规则：</p>
-        <p>1、增值税普通发票订单完成后1年内可补开发票，增值税专用发票订单完成10天后，1年内可以补开发票。</p>
-      </van-cell>
-    </van-cell-group>
-    <x-cell-group>
-      <x-cell
-        v-for="(item, index) in list"
-        :key="index"
-        :is-link="false"
-        :title="item.title"
-        :inlineDesc="item.label"
-      >
-      <template slot="value">
-        <van-button slot="button" size="small" type="primary" @click="apply">申请发票</van-button>
-      </template>
-      </x-cell>
-    </x-cell-group>
-  </div>
+	<div class>
+		<x-header title="发票服务"></x-header>
+		<van-cell-group>
+			<van-cell class="tips">
+				<h4>温馨提示：</h4>
+				<p>一、发票换开：</p>
+				<p>1、订单完成后一年内可支持换开发票一次。</p>
+				<p>2、不支持“企业抬头”变更。</p>
+				<p>二、补开发票规则：</p>
+				<p>
+					1、增值税普通发票订单完成后1年内可补开发票，增值税专用发票订单完成10天后，1年内可以补开发票。
+				</p>
+			</van-cell>
+		</van-cell-group>
+		<x-cell-group title="开票记录">
+			<x-cell
+				v-for="(item, index) in list"
+				:key="index"
+				:is-link="false"
+				:title="item.bill.name"
+				:value="'￥'+item.amount"
+				:inlineDesc="item.bill.mobile + '/' + item.created_at"
+			>
+			</x-cell>
+		</x-cell-group>
+		<infinite-loading @infinite="getBillrecord" spinner="spiral">
+			<div slot="no-more">没有更多数据啦...</div>
+			<div slot="no-results">没有数据</div>
+		</infinite-loading>
+		<div class="op" @click="apply()">
+			<x-button type="primary" text="开票申请"></x-button>
+		</div>
+	</div>
 </template>
 
 <script>
-import XHeader from "$components/XHeader";
-import Cell from "vant/lib/cell";
-import "vant/lib/cell/style";
-import CellGroup from "vant/lib/cell-group";
-import "vant/lib/cell-group/style";
-import XCell from "$components/XCell";
-import XCellGroup from "$components/XCellGroup";
-import Button from "vant/lib/button";
-import "vant/lib/button/style";
+	import XHeader from "$components/XHeader";
+	import Cell from "vant/lib/cell";
+	import "vant/lib/cell/style";
+	import CellGroup from "vant/lib/cell-group";
+	import "vant/lib/cell-group/style";
+	import XCell from "$components/XCell";
+	import XCellGroup from "$components/XCellGroup";
+	import XButton from "$components/XButton";
+	import InfiniteLoading from "vue-infinite-loading";
 
-export default {
-  //import引入的组件需要注入到对象中才能使用
-  components: {
-    XHeader,
-    "van-cell-group": CellGroup,
-    "van-cell": Cell,
-    "van-button": Button,
-    XCell,
-    XCellGroup,
-  },
-  data() {
-    //这里存放数据
-    return {
-      list: [
-        {
-          title: "2019-7-20充值",
-          label: "2019-7-20 12:00",
-        }
-      ]
-    };
-  },
-  computed: {},
-  watch: {},
-  methods: {
-    apply () {
-      this.$router.push({
-        name: 'wallet.invoiceinfo'
-      })
-    }
-  },
-  created() {},
-  mounted() {}
-};
+	export default {
+		//import引入的组件需要注入到对象中才能使用
+		components: {
+			XHeader,
+			"van-cell-group": CellGroup,
+			"van-cell": Cell,
+			XButton,
+			XCell,
+			XCellGroup,
+			InfiniteLoading
+		},
+		data () {
+			//这里存放数据
+			return {
+				list: [],
+				page: 1,
+			};
+		},
+		computed: {},
+		watch: {},
+		methods: {
+			apply () {
+				this.$router.push({
+					name: 'wallet.invoiceinfo'
+				})
+			},
+			getBillrecord ($state) {
+				this.$http.get('api/v2/alliance/bill/record', { params: { include: 'bill' } }).then(({ data }) => {
+					if (data.bills.data.length > 0) {
+						this.page += 1
+						this.list.push(...data.bills.data)
+						$state.loaded()
+					}
+					if (data.bills.per_page > data.bills.data.length) {
+						$state.complete();
+					}
+				})
+			}
+		},
+		created () { },
+		mounted () { }
+	};
 </script>
 <style lang='scss' scoped>
-//@import url(); 引入公共css类
-.tips {
-  p {
-    color: #999;
-    font-size: 12px;
-  }
-}
+	//@import url(); 引入公共css类
+	.tips {
+		p {
+			color: #999;
+			font-size: 12px;
+		}
+	}
+	.op {
+		width: 90%;
+		margin: 0 auto;
+	}
+	.op a {
+		height: 50px;
+		line-height: 50px;
+		font-size: 14px;
+	}
 </style>
--- src/modules/wallet/recharge.vue
+++ src/modules/wallet/recharge.vue
@@ -1,105 +1,134 @@
 <template>
-  <div class>
-    <x-header title="充值"></x-header>
-    <van-cell-group title="充值金额">
-      <van-field v-model="value" placeholder="请输入充值金额">
-        <span slot="button">元</span>
-      </van-field>
-    </van-cell-group>
-    <van-radio-group v-model="radio">
-      <van-cell-group title="选择支付方式">
-        <van-cell title="支付宝" clickable @click="radio = '1'" icon="alipay">
-          <van-radio slot="right-icon" name="1"/>
-        </van-cell>
-        <van-cell title="微信" clickable @click="radio = '2'" icon="wechat">
-          <van-radio slot="right-icon" name="2"/>
-        </van-cell>
-      </van-cell-group>
-    </van-radio-group>
-    <div class="agreement">
-      <label for="weuiAgree" class="weui-agree">
-        <input
-          id="weuiAgree"
-          type="checkbox"
-          v-model="isagree"
-          checked
-          class="weui-agree__checkbox"
-        >
-        <span class="weui-agree__text">
-          我已阅读并同意
-          <a @click="showAgree" href="javascript:void(0)">《充值协议》</a>
-        </span>
-      </label>
-    </div>
-     <div class="btn">
-      <van-button type="primary" hairline size="large" @click="next()">确认</van-button>
-    </div>
-  </div>
+	<div class>
+		<x-header title="充值"></x-header>
+		<van-cell-group title="充值金额">
+			<van-field v-model="value" placeholder="请输入充值金额">
+				<span slot="button">元</span>
+			</van-field>
+		</van-cell-group>
+		<van-radio-group v-model="radio">
+			<van-cell-group title="选择支付方式">
+				<van-cell title="支付宝" clickable @click="radio = '1'" icon="alipay">
+					<van-radio slot="right-icon" name="1" />
+				</van-cell>
+				<van-cell title="微信" clickable @click="radio = '2'" icon="wechat">
+					<van-radio slot="right-icon" name="2" />
+				</van-cell>
+			</van-cell-group>
+		</van-radio-group>
+		<div class="agreement">
+			<label for="weuiAgree" class="weui-agree">
+				<input
+					id="weuiAgree"
+					type="checkbox"
+					v-model="isagree"
+					checked
+					class="weui-agree__checkbox"
+				/>
+				<span class="weui-agree__text">
+					我已阅读并同意
+					<a @click="showAgree" href="javascript:void(0)">《充值协议》</a>
+				</span>
+			</label>
+		</div>
+		<div class="btn">
+			<van-button type="primary" hairline size="large" @click="next()"
+				>确认</van-button
+			>
+		</div>
+	</div>
 </template>
 
 <script>
-import XHeader from "$components/XHeader";
-import Cell from "vant/lib/cell";
-import "vant/lib/cell/style";
-import CellGroup from "vant/lib/cell-group";
-import "vant/lib/cell-group/style";
-import Icon from "vant/lib/icon";
-import "vant/lib/icon/style";
-import Field from "vant/lib/field";
-import "vant/lib/field/style";
-import RadioGroup from "vant/lib/radio-group";
-import "vant/lib/radio-group/style";
-import Radio from "vant/lib/radio";
-import "vant/lib/radio/style";
-import Button from "vant/lib/button";
-import "vant/lib/button/style";
-export default {
-  //import引入的组件需要注入到对象中才能使用
-  components: {
-    XHeader,
-    "van-cell-group": CellGroup,
-    "van-cell": Cell,
-    "van-icon": Icon,
-    "van-field": Field,
-    "van-radio-group": RadioGroup,
-    "van-radio": Radio,
-    "van-button": Button
-  },
-  data() {
-    //这里存放数据
-    return {
-      value: "3000.00",
-      radio: "1",
-      isagree: true
-    };
-  },
-  computed: {},
-  watch: {},
-  methods: {
-    showAgree() {
-      // this.$refs.agree.show();
-    }
-  },
-  created() {},
-  mounted() {}
-};
+	import XHeader from "$components/XHeader";
+	import Cell from "vant/lib/cell";
+	import "vant/lib/cell/style";
+	import CellGroup from "vant/lib/cell-group";
+	import "vant/lib/cell-group/style";
+	import Icon from "vant/lib/icon";
+	import "vant/lib/icon/style";
+	import Field from "vant/lib/field";
+	import "vant/lib/field/style";
+	import RadioGroup from "vant/lib/radio-group";
+	import "vant/lib/radio-group/style";
+	import Radio from "vant/lib/radio";
+	import "vant/lib/radio/style";
+	import Button from "vant/lib/button";
+	import "vant/lib/button/style";
+	import { mapGetters } from "vuex";
+	export default {
+		//import引入的组件需要注入到对象中才能使用
+		components: {
+			XHeader,
+			"van-cell-group": CellGroup,
+			"van-cell": Cell,
+			"van-icon": Icon,
+			"van-field": Field,
+			"van-radio-group": RadioGroup,
+			"van-radio": Radio,
+			"van-button": Button
+		},
+		data () {
+			//这里存放数据
+			return {
+				value: "3000.00",
+				radio: "1",
+				isagree: true,
+				name: '充值',
+        callback: 'http://localhost:8080/me/me',//待处理
+        result:'',
+			};
+		},
+		computed: {
+			...mapGetters(["currentUser"])
+		},
+		watch: {},
+		methods: {
+			showAgree () {
+				// this.$refs.agree.show();
+			},
+			next () {
+        if(this.value <= 0){
+          this.$alert('输入金额有误')
+          return false;
+        }
+        let params = {
+						user_id: this.$store.getters.currentUser.id,
+						order_name: this.name,
+						total_amount: this.value,
+						callback: this.callback
+					}
+				if (this.radio == 1) {
+					this.$http.post('api/v2/alliance/alipay', params).then(data => {
+            document.write(data)
+					})
+				} else if (this.radio == 2) {
+					this.$http.post('api/v2/alliance/wechat',params).then(data => {
+            console.log(data)
+					})
+				}
+			}
+		},
+		created () { },
+		mounted () { }
+	};
 </script>
 <style lang='scss' scoped>
-//@import url(); 引入公共css类
-.agreement {
-  text-align: center;
-  margin: 40px auto 15px;
-}
-.van-cell__left-icon{
-  font-size: 23px;
-}
-.btn {
-  width: 90%;
-  margin: auto;
-  .van-button--primary {
-    height: 48px;
-    line-height: 48px;
-    font-size: 14px;
-  }
-}
+	//@import url(); 引入公共css类
+	.agreement {
+		text-align: center;
+		margin: 40px auto 15px;
+	}
+	.van-cell__left-icon {
+		font-size: 23px;
+	}
+	.btn {
+		width: 90%;
+		margin: auto;
+		.van-button--primary {
+			height: 48px;
+			line-height: 48px;
+			font-size: 14px;
+		}
+	}
 </style>
